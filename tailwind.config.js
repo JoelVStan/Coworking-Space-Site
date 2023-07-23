497c1ba077d4b2d8efcd9fbf438fc9979dd35090
Joel V Stanley <101164781+JoelVStan@users.noreply.github.com>
2023-07-24 00:00:05 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Montserrat: ["Montserrat", "sans-serif"],
+      },
+      colors: {
+        'black': '#282828',
+      }
+    }, 
+  },
+  plugins: [],
+}
+
